ddb12674ae87ee2ec4f2dc3b737c5483d4c5dafb
abe03112133 <al03112133@tecmilenio.mx>
2025-10-30 00:09:10 -0600

    Clases Response y Request de la app

--- src/main/java/com/calculadora/model/OperacionRequest.java
+++ src/main/java/com/calculadora/model/OperacionRequest.java
@@ -1,4 +1,59 @@
 package com.calculadora.model;
 
+/**
+ * Clase modelo para las peticiones de operaciones
+ * Contiene los dos números que se van a operar
+ *
+ * Historia de Usuario: H1 - Aplicación web con Spring Boot
+ * Desarrollado por: Luis Ramírez
+ *
+ * @author Luis Ramírez
+ * @since 2024-10-22
+ */
 public class OperacionRequest {
+
+    private double numero1;
+    private double numero2;
+
+    /**
+     * Constructor por defecto
+     */
+    public OperacionRequest() {
+    }
+
+    /**
+     * Constructor con parámetros
+     * @param numero1 Primer número de la operación
+     * @param numero2 Segundo número de la operación
+     */
+    public OperacionRequest(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    // Getters y Setters
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    @Override
+    public String toString() {
+        return "OperacionRequest{" +
+                "numero1=" + numero1 +
+                ", numero2=" + numero2 +
+                '}';
+    }
 }
--- src/main/java/com/calculadora/model/OperacionResponse.java
+++ src/main/java/com/calculadora/model/OperacionResponse.java
@@ -1,4 +1,91 @@
 package com.calculadora.model;
 
+/**
+ * Clase modelo para las respuestas de operaciones
+ * Contiene el resultado y el tipo de operación realizada
+ *
+ * Historia de Usuario: H1 - Aplicación web con Spring Boot
+ * Desarrollado por: Luis Ramírez
+ *
+ * @author Luis Ramírez
+ * @since 2024-10-22
+ */
 public class OperacionResponse {
+
+    private double resultado;
+    private String operacion;
+    private boolean exito;
+    private String mensaje;
+
+    /**
+     * Constructor por defecto
+     */
+    public OperacionResponse() {
+    }
+
+    /**
+     * Constructor para respuestas exitosas
+     * @param resultado Resultado de la operación
+     * @param operacion Tipo de operación (suma, resta, etc.)
+     */
+    public OperacionResponse(double resultado, String operacion) {
+        this.resultado = resultado;
+        this.operacion = operacion;
+        this.exito = true;
+        this.mensaje = "Operación realizada exitosamente";
+    }
+
+    /**
+     * Constructor para respuestas con error
+     * @param exito Estado de la operación
+     * @param mensaje Mensaje de error
+     */
+    public OperacionResponse(boolean exito, String mensaje) {
+        this.exito = exito;
+        this.mensaje = mensaje;
+    }
+
+    // Getters y Setters
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public boolean isExito() {
+        return exito;
+    }
+
+    public void setExito(boolean exito) {
+        this.exito = exito;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String toString() {
+        return "OperacionResponse{" +
+                "resultado=" + resultado +
+                ", operacion='" + operacion + '\'' +
+                ", exito=" + exito +
+                ", mensaje='" + mensaje + '\'' +
+                '}';
+    }
 }
